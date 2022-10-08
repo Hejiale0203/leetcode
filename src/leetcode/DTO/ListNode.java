f42fabdd2beb1097aa132ecfff6028468f44aeee
Hejiale970203 <982364643@qq.com>
2022-10-08 22:29:01 +0800

    2

---
+++ src/leetcode/DTO/ListNode.java
@@ -0,0 +1,22 @@
+package leetcode.DTO;
+
+/**
+ * @author hjl
+ * @Date 2022/10/8 20:33
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
